7279264ecb52ae21e847df17df8809653fc1e108
cadit <cadit@DESKTOP-PCOVD1I>
2020-04-17 12:12:31 +0200

    base de datos no guarda en tabla

---
+++ src/main/java/es/isabeljaimeatienza/catalogoseries/CatalogoSeries.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.isabeljaimeatienza.catalogoseries;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author cadit
+ */
+public class CatalogoSeries {
+
+    public static void main(String[] args) {
+        // Conectar con la base de datos
+        Map<String, String> emfProperties = new HashMap<>();
+        emfProperties.put("javax.persistence.schema-generation.database.action", "create");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("CatalogoSeriesPU", emfProperties);
+        EntityManager em = emf.createEntityManager();
+
+        // REALIZAR AQUÍ LAS OPERACIONES SOBRE LA BASE DE DATOS
+        em.getTransaction().begin();
+        Genero generoRomance = new Genero(null, "Romance");
+        Genero generoComedia = new Genero();
+        generoComedia.setNombre("Comedia");
+        Genero generoTerror = new Genero();
+        generoTerror.setNombre("Terror");
+        Genero generoSuspense = new Genero();
+        generoSuspense.setNombre("Suspense");
+        
+       
+        em.persist(generoRomance);
+        em.persist(generoComedia);
+        em.persist(generoTerror);
+        em.persist(generoSuspense);
+        em.getTransaction().commit();
+        // Cerrar la conexión con la base de datos
+        em.close();
+        emf.close();
+        try {
+            DriverManager.getConnection("jdbc:derby:CatalogoSeries;shutdown=true");
+        } catch (SQLException ex) {
+        }
+    }
+}
---
+++ src/main/java/es/isabeljaimeatienza/catalogoseries/Serie.java
@@ -0,0 +1,179 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.isabeljaimeatienza.catalogoseries;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author cadit
+ */
+@Entity
+@Table(name = "SERIE")
+@NamedQueries({
+    @NamedQuery(name = "Serie.findAll", query = "SELECT s FROM Serie s"),
+    @NamedQuery(name = "Serie.findById", query = "SELECT s FROM Serie s WHERE s.id = :id"),
+    @NamedQuery(name = "Serie.findByT\u00edtulo", query = "SELECT s FROM Serie s WHERE s.t\u00edtulo = :t\u00edtulo"),
+    @NamedQuery(name = "Serie.findByFechaEstreno", query = "SELECT s FROM Serie s WHERE s.fechaEstreno = :fechaEstreno"),
+    @NamedQuery(name = "Serie.findByCapitulos", query = "SELECT s FROM Serie s WHERE s.capitulos = :capitulos"),
+    @NamedQuery(name = "Serie.findByPrecio", query = "SELECT s FROM Serie s WHERE s.precio = :precio"),
+    @NamedQuery(name = "Serie.findByValoracion", query = "SELECT s FROM Serie s WHERE s.valoracion = :valoracion"),
+    @NamedQuery(name = "Serie.findByVisionada", query = "SELECT s FROM Serie s WHERE s.visionada = :visionada"),
+    @NamedQuery(name = "Serie.findByFoto", query = "SELECT s FROM Serie s WHERE s.foto = :foto")})
+public class Serie implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "T\u00cdTULO")
+    private String título;
+    @Column(name = "FECHA_ESTRENO")
+    @Temporal(TemporalType.DATE)
+    private Date fechaEstreno;
+    @Column(name = "CAPITULOS")
+    private Short capitulos;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "PRECIO")
+    private BigDecimal precio;
+    @Column(name = "VALORACION")
+    private Boolean valoracion;
+    @Column(name = "VISIONADA")
+    private Boolean visionada;
+    @Column(name = "FOTO")
+    private String foto;
+    @JoinColumn(name = "GENERO", referencedColumnName = "ID")
+    @ManyToOne(optional = false)
+    private Genero genero;
+
+    public Serie() {
+    }
+
+    public Serie(Integer id) {
+        this.id = id;
+    }
+
+    public Serie(Integer id, String título) {
+        this.id = id;
+        this.título = título;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTítulo() {
+        return título;
+    }
+
+    public void setTítulo(String título) {
+        this.título = título;
+    }
+
+    public Date getFechaEstreno() {
+        return fechaEstreno;
+    }
+
+    public void setFechaEstreno(Date fechaEstreno) {
+        this.fechaEstreno = fechaEstreno;
+    }
+
+    public Short getCapitulos() {
+        return capitulos;
+    }
+
+    public void setCapitulos(Short capitulos) {
+        this.capitulos = capitulos;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
+    public Boolean getValoracion() {
+        return valoracion;
+    }
+
+    public void setValoracion(Boolean valoracion) {
+        this.valoracion = valoracion;
+    }
+
+    public Boolean getVisionada() {
+        return visionada;
+    }
+
+    public void setVisionada(Boolean visionada) {
+        this.visionada = visionada;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public Genero getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Genero genero) {
+        this.genero = genero;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Serie)) {
+            return false;
+        }
+        Serie other = (Serie) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "es.isabeljaimeatienza.catalogoseries.Serie[ id=" + id + " ]";
+    }
+    
+}
